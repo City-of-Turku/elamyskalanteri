17b7ba586b0c3f864424812edef47845a1e22c50
Essi-Maria Varjonen <essi@weasel.fi>
2022-03-14 14:59:58 +0200

    event content changes

--- react-ui/src/components/pages/events/EventContent.tsx
+++ react-ui/src/components/pages/events/EventContent.tsx
@@ -1,8 +1,9 @@
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import LinkIcon from "@mui/icons-material/Link";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
-import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
+import Grid from "@mui/material/Grid";
+import { styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
 import dayjs from "dayjs";
 import { Link, useParams } from "react-router-dom";
@@ -10,56 +11,50 @@ import { useEventQuery } from "../../../redux/services/eventApi";
 
 dayjs.locale("fi");
 
+const Img = styled("img")({
+  maxWidth: "100%",
+  maxHeight: "100%",
+});
+
 const EventContent = () => {
   const params: any = useParams();
   const { data, isLoading, isFetching, error } = useEventQuery(params?.id);
 
   return (
     <div>
-      <div id="block1">
-        <Box sx={{ p: 2 }}>
-          <Link to="/" style={{ textDecoration: "none", color: "black" }}>
-            <ArrowBackIcon />
-          </Link>
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: { xs: "center", md: "center" },
-          }}
-        >
-          <Box
-            component="img"
-            sx={{
-              m: 2,
-              width: "100%",
-              maxWidth: 906,
-            }}
-            alt="The house from the offer."
-            src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"
-          ></Box>
-
-          <Box
-            component="div"
-            sx={{
-              width: "58%",
-              display: "flex",
-              flexDirection: "column",
-              maxWidth: 906,
-            }}
-          >
-            <Typography variant="h6" component="div">
-              {dayjs(data?.start_time).format("LLL")}
-            </Typography>
-            <Typography
-              variant="h4"
-              component="div"
-              sx={{ fontWeight: "bold", pb: 2 }}
-            >
-              {data?.name?.fi}
-            </Typography>
-            <Box component="div">
+      <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+        <ArrowBackIcon />
+      </Link>
+      <Grid
+        item
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: { xs: "center", md: "center" },
+        }}
+      >
+        <Img
+          alt="complex"
+          src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"
+        />
+      </Grid>
+      <Grid container spacing={5}>
+        <Grid item xs={20} sm container>
+          <Grid item xs container direction="column" spacing={2} p={5}>
+            <Grid item xs={2}>
+              <Typography
+                component="div"
+                sx={{ fontSize: 18, pb: 2, fontWeight: "bold" }}
+              >
+                {dayjs(data?.start_time).format("LLL")}
+              </Typography>
+              <Typography
+                variant="h4"
+                component="div"
+                sx={{ fontWeight: "bold", pb: 2 }}
+              >
+                {data?.name?.fi}
+              </Typography>
               <Typography
                 sx={{ display: "flex", flexDirection: "row", pb: 1 }}
                 variant="body2"
@@ -68,9 +63,13 @@ const EventContent = () => {
                 <LocationOnIcon color="action" fontSize="small" />
                 &nbsp;{data?.provider?.fi}
               </Typography>
-
               <Typography
-                sx={{ display: "flex", flexDirection: "row", pb: 1 }}
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  pb: 1,
+                  cursor: "pointer",
+                }}
                 variant="body2"
                 color="text.secondary"
               >
@@ -78,28 +77,69 @@ const EventContent = () => {
                 {/* {currentEvent?.info_url.fi} */}
                 &nbsp; www.testi.fi
               </Typography>
-            </Box>
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              maxWidth: 906,
-              p: 4,
-            }}
-          >
+
+              <Grid item>
+                <Typography
+                  sx={{
+                    fontWeight: "bold",
+                    fontSize: 15,
+                    maxWidth: 800,
+                    pb: 2,
+                    letterSpacing: 1,
+                  }}
+                  variant="body2"
+                >
+                  {data?.short_description?.fi}
+                </Typography>
+                <Divider textAlign="left" />
+                <Typography
+                  sx={{
+                    fontWeight: "light",
+                    fontSize: "default",
+                    maxWidth: 800,
+                    pt: 2,
+                    letterSpacing: 1,
+                  }}
+                  variant="body2"
+                >
+                  {data?.description?.fi}
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+
+          <Grid item p={5}>
             <Typography sx={{ fontWeight: "bold", fontSize: "default" }}>
-              {data?.short_description?.fi}
+              Hinta
             </Typography>
-            <br></br>
-            <Divider textAlign="left" />
-            <br></br>
-            <Typography sx={{ fontWeight: "light", fontSize: "default" }}>
-              {data?.description?.fi}
+            <Typography variant="subtitle1" component="div">
+              Liput 12€
             </Typography>
-          </Box>
-        </Box>
-      </div>
+            <Typography sx={{ fontWeight: "bold", fontSize: "default" }}>
+              Ikäraja
+            </Typography>
+            <Typography variant="subtitle1" component="div">
+              Alle 12 v.
+            </Typography>
+            <Typography sx={{ fontWeight: "bold", fontSize: "default" }}>
+              Järjestäjä
+            </Typography>
+            <Typography variant="subtitle1" component="div">
+              Lorem ipsum Ry.
+            </Typography>
+            <Typography sx={{ fontWeight: "bold", fontSize: "default" }}>
+              Tutustu lisää
+            </Typography>
+            <Typography variant="subtitle1" component="div">
+              Facebook Instagram Twitter Video
+            </Typography>
+            <Typography sx={{ fontWeight: "bold", fontSize: "default" }}>
+              Jaa kaverille
+            </Typography>
+            <Typography variant="subtitle1" component="div"></Typography>
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   );
 };
