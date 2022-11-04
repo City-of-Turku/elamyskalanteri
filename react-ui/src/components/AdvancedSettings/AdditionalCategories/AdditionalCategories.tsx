da5150e3a94f5a821606bbee9ef4a680a5ea4cd1
Johannes Lindqvist <johannes@weasel.fi>
2022-11-04 12:50:15 +0200

    Advanced search additional categories updates embed code

--- react-ui/src/components/AdvancedSettings/AdditionalCategories/AdditionalCategories.tsx
+++ react-ui/src/components/AdvancedSettings/AdditionalCategories/AdditionalCategories.tsx
@@ -1,16 +1,32 @@
-import * as React from 'react';
+import { bindActionCreators } from "@reduxjs/toolkit";
+import { useAppDispatch } from "../../../hooks/rtkHooks";
+
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
+import filterSlice from "../../../redux/slices/filterSlice";
 
 const AdditionalCategories = () => {
+    const dispatch = useAppDispatch()
+    const { addEventType, removeEventTypes } = bindActionCreators(filterSlice.actions, dispatch)
+
+    const handleChange = (e:any) => {
+      if (e.target.checked) {
+        // @ts-ignore
+        addEventType(e.target.value)
+      }
+      else {
+        // @ts-ignore
+        removeEventTypes([e.target.value])
+      }
+    }
     return (
         <div>
             <p>Lisäluokat</p>
         <FormGroup sx={{fontFamily: "Forma DJR Micro"}}>
-          <FormControlLabel control={<Checkbox />} label="MiHi" />
-          <FormControlLabel control={<Checkbox />} label="Kivakesä" />
-          <FormControlLabel control={<Checkbox />} label="Kivaloma" />
+          <FormControlLabel control={<Checkbox />} label="MiHi" value="system:extra:mihi" onChange={event => handleChange(event)} />
+          <FormControlLabel control={<Checkbox />} label="Kivakesä" value="system:extra:kivakesa" onChange={event => handleChange(event)} />
+          <FormControlLabel control={<Checkbox />} label="Kivaloma" value="system:extra:kivaloma" onChange={event => handleChange(event)} />
         </FormGroup>
         </div>
       );
