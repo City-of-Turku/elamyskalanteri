9c46f233c86b900a80ffac4c2f5e1fba8b9c69c2
Juha Kujala <juha.kujala@anders.fi>
2023-02-06 10:45:01 +0200

    Update WhatContainer

--- react-ui/src/components/FilterContainer/WhatContainer/CategorySelector.tsx
+++ react-ui/src/components/FilterContainer/WhatContainer/CategorySelector.tsx
@@ -1,58 +1,34 @@
-import { CircularProgress } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { getTranslatedValue } from '../../../functions/getTranslatedValue';
+import { Category } from '../../../types';
 import FilterChip from '../FilterChip/FilterChip';
 import styles from './WhatContainer.module.css';
 
-export interface ICategory {
-  name: {
-    fi: string;
-    en: string;
-    sv: string;
-  };
-  yso: string;
-}
-
-interface ICategorySelectorProps {
-  title: string;
-  categories: ICategory[];
-  isLoading: boolean;
+export type ICategorySelectorProps = {
+  categories: Category[];
   selected: string[];
-  handleDelete(yso: string): any;
-  handleAdd(yso: string): any;
-}
+  handleDelete: (yso: string) => void;
+  handleAdd: (yso: string) => void;
+};
 
 export const CategorySelector = (props: ICategorySelectorProps) => {
+  const { categories, selected, handleAdd, handleDelete } = props;
   const { i18n } = useTranslation();
-  const theme = useTheme();
+
+  if (!categories) return null;
 
   return (
-    <>
-      <p
-        style={{
-          color: theme.palette.primary.dark,
-          fontSize: 18,
-          fontFamily: 'halogen, sans-serif',
-          fontWeight: 900,
-          //textTransform: "capitalize",
-        }}
-      >
-        <b>{props.title}</b>
-      </p>
-      <div className={styles.chipContainer}>
-        {props.isLoading && <CircularProgress />}
-        {props.categories &&
-          props.categories.map((category: ICategory) => (
-            <FilterChip
-              key={category.yso}
-              label={category.name[i18n.language as keyof typeof category.name]}
-              active={props.selected.includes(category.yso)}
-              handleClick={() => props.handleAdd(category.yso)}
-              handleDelete={() => props.handleDelete(category.yso)}
-            />
-          ))}
-      </div>
-    </>
+    <div className={styles.chipContainer}>
+      {categories.map((category) => (
+        <FilterChip
+          key={category.yso}
+          label={getTranslatedValue(category.name, i18n.language) || ''}
+          active={selected.includes(category.yso)}
+          handleClick={() => handleAdd(category.yso)}
+          handleDelete={() => handleDelete(category.yso)}
+        />
+      ))}
+    </div>
   );
 };
