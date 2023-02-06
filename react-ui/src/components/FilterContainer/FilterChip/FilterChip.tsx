5f0d24ad31f2a86b6d5109e9f58e962e963c8a2f
Juha Kujala <juha.kujala@anders.fi>
2023-02-06 10:47:00 +0200

    Update styling on FilterChip

--- react-ui/src/components/FilterContainer/FilterChip/FilterChip.tsx
+++ react-ui/src/components/FilterContainer/FilterChip/FilterChip.tsx
@@ -1,4 +1,4 @@
-import { Chip, Typography } from '@mui/material';
+import { alpha, Chip, Typography } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import React from 'react';
 import styles from './FilterChip.module.css';
@@ -22,26 +22,23 @@ const FilterChip = ({ label, active, handleClick, handleDelete }: IProps) => {
     <>
       {active ? (
         <Chip
-          label={
-            <Typography sx={{ fontFamily: 'forma-djr-micro, sans-serif' }}>{label}</Typography>
-          }
+          label={<Typography sx={{ color: 'inherit' }}>{label}</Typography>}
           variant={active ? 'filled' : 'outlined'}
           className={styles.chip}
-          color="primary"
-          sx={{ backgroundColor: theme.palette.primary.dark, color: '#ffffff', fontSize: 16 }}
+          color="secondary"
+          sx={{ backgroundColor: theme.palette.secondary.main, color: '#fff', fontSize: 16 }}
           onDelete={() => (handleDelete ? handleDelete() : null)}
         />
       ) : (
         <Chip
-          label={
-            <Typography sx={{ fontFamily: 'forma-djr-micro, sans-serif' }}>{label}</Typography>
-          }
+          label={<Typography sx={{ color: 'inherit' }}>{label}</Typography>}
           variant={active ? 'filled' : 'outlined'}
           onClick={() => (handleClick ? handleClick() : null)}
           className={styles.chip}
           sx={{
-            color: theme.palette.primary.dark,
-            backgroundColor: 'rgba(242, 202, 153, 0.2);',
+            color: theme.palette.secondary.main,
+            // backgroundColor: 'rgba(242, 202, 153, 0.2);',
+            backgroundColor: (theme) => alpha(theme.palette.primary.light, 0.08),
             fontSize: 16,
           }}
         />
