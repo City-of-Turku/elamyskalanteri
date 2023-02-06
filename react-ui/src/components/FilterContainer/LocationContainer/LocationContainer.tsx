01dd1b65aeb980c72ddcdfa1a324de5a44beed6b
Juha Kujala <juha.kujala@anders.fi>
2023-02-06 10:47:32 +0200

    Fix types for LocationContainer

--- react-ui/src/components/FilterContainer/LocationContainer/LocationContainer.tsx
+++ react-ui/src/components/FilterContainer/LocationContainer/LocationContainer.tsx
@@ -1,14 +1,12 @@
 import { Checkbox, FormControlLabel, FormGroup, Typography } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
 import { bindActionCreators } from '@reduxjs/toolkit';
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { getCoords } from '../../../functions/boundingBox';
+import { getCoords, Position } from '../../../functions/boundingBox';
 import { useAppDispatch } from '../../../hooks/rtkHooks';
 import filterSlice from '../../../redux/slices/filterSlice';
 
 const LocationContainer = () => {
-  const theme = useTheme();
   const dispatch = useAppDispatch();
 
   const { t } = useTranslation();
@@ -26,7 +24,7 @@ const LocationContainer = () => {
       { timeout: 5000 },
     );
 
-    const handleCoords = (pos: { coords: any }, d: number) => {
+    const handleCoords = (pos: Position, d: number) => {
       const coords = getCoords(pos, d);
       setBbox(coords);
     };
@@ -34,101 +32,89 @@ const LocationContainer = () => {
 
   return (
     <div>
-      <b
-        style={{
-          color: theme.palette.primary.dark,
-          fontSize: 18,
-          fontFamily: 'halogen, sans-serif',
-          fontWeight: 900,
-          textTransform: 'capitalize',
-        }}
-      >
-        <p>{t('near')}</p>
-      </b>
+      <Typography variant="h3" style={{ margin: '0 0 16px 0' }}>
+        {t('near')}
+      </Typography>
       <FormGroup row>
         <FormControlLabel
-          control={<Checkbox />}
-          label={
-            <Typography sx={{ fontFamily: 'forma-djr-micro, sans-serif' }}>{`${t(
-              'under',
-            )} 1 km`}</Typography>
+          control={
+            <Checkbox
+              checked={activeCheckbox === 1}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                if (!e.target.checked) {
+                  setActiveCheckbox(null);
+                  setBbox({ north: null, east: null, south: null, west: null });
+                  return;
+                }
+                setActiveCheckbox(Number(e.target.value));
+                handleLocation(1);
+              }}
+            />
           }
+          label={`${t('under')} 1 km`}
           labelPlacement={'end'}
-          style={{ width: '140px' }}
+          style={{ width: '140px', textTransform: 'capitalize' }}
           value={1}
-          checked={activeCheckbox === 1}
-          onChange={(e: any) => {
-            if (!e.target.checked) {
-              setActiveCheckbox(null);
-              setBbox({ north: null, east: null, south: null, west: null });
-              return;
-            }
-            setActiveCheckbox(Number(e.target.value));
-            handleLocation(1);
-          }}
         />
         <FormControlLabel
-          control={<Checkbox />}
-          label={
-            <Typography sx={{ fontFamily: 'forma-djr-micro, sans-serif' }}>{`${t(
-              'under',
-            )} 3 km`}</Typography>
+          control={
+            <Checkbox
+              checked={activeCheckbox === 2}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                if (!e.target.checked) {
+                  setActiveCheckbox(null);
+                  setBbox({ north: null, east: null, south: null, west: null });
+                  return;
+                }
+                setActiveCheckbox(Number(e.target.value));
+                handleLocation(3);
+              }}
+            />
           }
+          label={`${t('under')} 3 km`}
           labelPlacement={'end'}
-          style={{ width: '140px' }}
+          style={{ width: '140px', textTransform: 'capitalize' }}
           value={2}
-          checked={activeCheckbox === 2}
-          onChange={(e: any) => {
-            if (!e.target.checked) {
-              setActiveCheckbox(null);
-              setBbox({ north: null, east: null, south: null, west: null });
-              return;
-            }
-            setActiveCheckbox(Number(e.target.value));
-            handleLocation(3);
-          }}
         />
         <FormControlLabel
-          control={<Checkbox />}
-          label={
-            <Typography sx={{ fontFamily: 'forma-djr-micro, sans-serif' }}>{`${t(
-              'under',
-            )} 5 km`}</Typography>
+          control={
+            <Checkbox
+              checked={activeCheckbox === 3}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                if (!e.target.checked) {
+                  setActiveCheckbox(null);
+                  setBbox({ north: null, east: null, south: null, west: null });
+                  return;
+                }
+                setActiveCheckbox(Number(e.target.value));
+                handleLocation(5);
+              }}
+            />
           }
+          label={`${t('under')} 5 km`}
           labelPlacement={'end'}
-          style={{ width: '140px' }}
+          style={{ width: '140px', textTransform: 'capitalize' }}
           value={3}
-          checked={activeCheckbox === 3}
-          onChange={(e: any) => {
-            if (!e.target.checked) {
-              setActiveCheckbox(null);
-              setBbox({ north: null, east: null, south: null, west: null });
-              return;
-            }
-            setActiveCheckbox(Number(e.target.value));
-            handleLocation(5);
-          }}
         />
         <FormControlLabel
-          control={<Checkbox />}
-          label={
-            <Typography sx={{ fontFamily: 'forma-djr-micro, sans-serif' }}>{`${t(
-              'under',
-            )} 10 km`}</Typography>
+          control={
+            <Checkbox
+              checked={activeCheckbox === 4}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                if (!e.target.checked) {
+                  setActiveCheckbox(null);
+                  setBbox({ north: null, east: null, south: null, west: null });
+                  return;
+                }
+                setActiveCheckbox(Number(e.target.value));
+                handleLocation(10);
+              }}
+            />
           }
+          label={`${t('under')} 10 km`}
           labelPlacement={'end'}
-          style={{ width: '140px' }}
+          style={{ width: '140px', textTransform: 'capitalize' }}
           value={4}
-          checked={activeCheckbox === 4}
-          onChange={(e: any) => {
-            if (!e.target.checked) {
-              setActiveCheckbox(null);
-              setBbox({ north: null, east: null, south: null, west: null });
-              return;
-            }
-            setActiveCheckbox(Number(e.target.value));
-            handleLocation(10);
-          }}
         />
       </FormGroup>
     </div>
