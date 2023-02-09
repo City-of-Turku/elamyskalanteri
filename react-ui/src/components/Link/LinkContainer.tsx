ac39e2f431a7bba4eb4aea039938368c1ad5783e
Juha Kujala <juha.kujala@anders.fi>
2023-02-09 21:50:57 +0200

    Don't render empty LinkContainer content

--- react-ui/src/components/Link/LinkContainer.tsx
+++ react-ui/src/components/Link/LinkContainer.tsx
@@ -6,19 +6,17 @@ import { ReactComponent as Thumbnail } from '../../svg/thumbnail.svg';
 const LinkContainer = () => {
   const options = useAppSelector((state) => state.options);
 
+  if (!options.linkContainer || !options.linkText) return null;
+
   return (
-    <>
-      <div style={{ display: 'flex', justifyContent: 'center', paddingTop: '4rem' }}>
-        {options.linkContainer && <Thumbnail width={88} height={33} />}
-        <div style={{ padding: '0.2rem' }}>
-          {options.linkContainer && (
-            <Link href={options.linkContainer} target="_blank">
-              {options.linkText && options.linkText}
-            </Link>
-          )}
-        </div>
+    <div style={{ display: 'flex', justifyContent: 'center', paddingTop: '4rem' }}>
+      <Thumbnail width={88} height={33} />
+      <div style={{ padding: '0.2rem' }}>
+        <Link href={options.linkContainer} target="_blank">
+          {options.linkText}
+        </Link>
       </div>
-    </>
+    </div>
   );
 };
 
